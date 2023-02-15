8d1c8684f5e89a724f2998c8b5d911fcb7dd3403
topcircle <topcircle@gmail.com>
2023-02-15 16:38:42 +0900

    2.0 Pattertns : Layouts

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,12 @@
+import Layout from "@/components/Layout";
 import NavBar from "@/components/NavBar";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <NavBar />
-      <Component {...pageProps} />
-      <style jsx global>{`
-        a {
-          color: white;
-        }
-      `}</style>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </>
   );
 }
