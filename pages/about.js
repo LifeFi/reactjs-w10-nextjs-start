56aec0faedc1426f061f67e1d9d615d0514eee84
topcircle <topcircle@gmail.com>
2023-02-15 15:46:51 +0900

    1.4

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,9 @@
+import NavBar from "@/components/NavBar";
+
 export default function About() {
   return (
     <div>
+      <NavBar />
       <h1>About us</h1>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,10 @@
-import { useState } from "react";
+import NavBar from "@/components/NavBar";
 
 export default function Home() {
-  const [count, setCount] = useState(0);
-  const onClick = () => setCount((prev) => prev + 1);
-
   return (
     <div>
-      <h1>{count}</h1>
-      <button onClick={onClick}>Click</button>
+      <NavBar />
+      <h1>Hello</h1>
     </div>
   );
 }
